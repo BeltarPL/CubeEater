491c7f321ae19fc305834b71199ff4f2b659fc6b
Grzegorz Swół <swol.grzegorz@gmail.com>
2024-02-24 18:18:45 +0100

    added list filtering and rescaled interface

--- Assets/_CubeEater/Scripts/InterfaceManager.cs
+++ Assets/_CubeEater/Scripts/InterfaceManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,9 @@ public class InterfaceManager : MonoBehaviour
 {
     [SerializeField] private ListContentManager listContentManager;
     [SerializeField] private Transform listContentParent;
+    [SerializeField] private TMP_InputField filterInputField;
+
+    private readonly List<Transform> listContent = new();
     
     private void Start()
     {
@@ -18,13 +22,25 @@ public class InterfaceManager : MonoBehaviour
         foreach (Element element in listContentManager.listContent)
         {
             Transform newElement = Instantiate(element.listElementPrefab, listContentParent);
-            newElement.name = element.name;
+            newElement.name = element.name.ToLower();
             newElement.GetComponent<Button>().onClick.AddListener(() => InstantiateSceneElement(element.sceneElementPrefab));
+            
+            listContent.Add(newElement);
         }
     }
 
     private void InstantiateSceneElement(Transform element)
     {
         Instantiate(element);
+    }
+    
+    public void FilterList()
+    {
+        string filter = filterInputField.text.ToLower();
+
+        foreach (Transform element in listContent)
+        {
+            element.gameObject.SetActive(element.name.Contains(filter));
+        }
     }
 }
